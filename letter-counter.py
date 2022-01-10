3650dac9069d33ef416ee6d3be63e033f6488d23
Magdalena-Piatkowska <magda.piatk@gmail.com>
2022-01-10 18:53:01 +0000

    cleaning up formatting

--- letter-counter.py
+++ letter-counter.py
@@ -1,4 +1,4 @@
-#Counts the number of times a letter appears in a string. Inspired by SoloLearn's "Beginner Python" course.
+# Counts the number of times a letter appears in a string. Inspired by SoloLearn's "Python for Beginners" course.
 
 print("Let's take a word and a letter, and check how many times said letter appears in said word. \n")
 word = str(input("Please provide a word: \n"))
--- sum-of-consecutive-numbers.py
+++ sum-of-consecutive-numbers.py
@@ -1,20 +1,20 @@
-#Counts the sum of consecutive numbers. Inspired by SoloLearn's "Beginner Python" course.
+# Counts the sum of consecutive numbers. Inspired by SoloLearn's "Python for Beginners" course.
 
 print("Let's take a number N and output the sum of all numbers from 1 to N (including N). \n")
 N = int(input("Please provide a number: \n"))
 
 
-#1st solution
+# 1st solution
 print(sum(range(1, N+1)))
 
 
-#2nd solution
-#count = 0
-#result = 0
-#operative = 0
+# 2nd solution
+# count = 0
+# result = 0
+# operative = 0
 #
-#for operative in range(1, N+1):
-#    count += 1
-#    result += count
+# for operative in range(1, N+1):
+#     count += 1
+#     result += count
 #
-#print(result)
+# print(result)
